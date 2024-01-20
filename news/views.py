f4541f79613a6f9388740ec0e11c15c8e106053a
Danillo Santos <dnashy@hotmail.com>
2024-01-20 11:39:30 -0300

    feat: add the categories form

--- news/views.py
+++ news/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from news.models import News
+from django.shortcuts import render, redirect
+from news.models import News, Category
+from news.forms import CreateCategoryForm
 
 
 def index(request):
@@ -10,3 +11,16 @@ def index(request):
 def news_details(request, id):
     context = {"news": News.objects.get(id=id)}
     return render(request, "news_details.html", context)
+
+
+def categories(request):
+    form = CreateCategoryForm()
+
+    if request.method == "POST":
+        form = CreateCategoryForm(request.POST)
+        if form.is_valid():
+            Category.objects.create(**form.cleaned_data)
+            return redirect("home-page")
+
+    context = {"form": form}
+    return render(request, "categories_form.html", context)
