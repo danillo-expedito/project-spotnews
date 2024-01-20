cfc60ef23b4fa393283ae27f38148ae98418a0fb
Danillo Santos <dnashy@hotmail.com>
2024-01-20 11:20:11 -0300

    feat: add the news details page template

--- news/views.py
+++ news/views.py
@@ -5,3 +5,8 @@ from news.models import News
 def index(request):
     context = {"news": News.objects.all()}
     return render(request, "home.html", context)
+
+
+def news_details(request, id):
+    context = {"news": News.objects.get(id=id)}
+    return render(request, "news_details.html", context)
