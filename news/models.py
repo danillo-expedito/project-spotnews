b3cd975597017624e68f7b8ce89b5fed45754286
Danillo Santos <dnashy@hotmail.com>
2024-01-20 10:08:18 -0300

    feat: add the News model

--- news/models.py
+++ news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 
 class Category(models.Model):
@@ -16,3 +17,27 @@ class User(models.Model):
 
     def __str__(self):
         return self.name
+
+
+def validate_min_two_words(value):
+    if len(value.split()) < 2:
+        raise ValidationError("O título deve conter pelo menos 2 palavras.")
+
+
+class News(models.Model):
+    title = models.CharField(
+        max_length=200,
+        null=False,
+        blank=False,
+        validators=[validate_min_two_words],
+    )
+    content = models.TextField(null=False, blank=False)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="news"
+    )
+    created_at = models.DateField()
+    image = models.ImageField(upload_to="img/", blank=True)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.title
